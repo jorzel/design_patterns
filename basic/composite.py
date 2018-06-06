10f0c1c7976e014e80a3459a62c59b3f748112dd
jorzel <jaroslaw.orzel@emplocity.pl>
2018-06-06 09:59:32 +0200

    Composite added

---
+++ basic/composite.py
@@ -0,0 +1,50 @@
+"""Usefult in Tree model structure"""
+
+class Component(object):
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def component_function(self):
+        pass
+
+class Child(Component):
+    def __init__(self, *args, **kwargs):
+        super(Child, self).__init__(*args, **kwargs)
+        self.name = args[0]
+    
+    def component_function(self):
+        print("{}".format(self.name))
+
+class Composite(Component):
+    def __init__(self, *args, **kwargs):
+        super(Composite, self).__init__(*args, **kwargs)
+        self.name = args[0]
+        self.children = []    
+
+    def append_child(self, child):
+        self.children.append(child)
+
+    def remove_child(self, child):
+        self.children.remove(child)
+    
+    def component_function(self):
+        print("{}".format(self.name))
+        for i in self.children:
+            i.component_function()
+        
+sub1 = Composite("submenu 1")
+sub11 = Child("sub_submenu 11")
+sub12 = Child("sub_submenu 12")
+
+sub1.append_child(sub11)
+sub1.append_child(sub12)
+
+top = Composite("top_menu")
+
+sub2 = Child("submenu 2")
+
+top.append_child(sub1)
+
+top.append_child(sub2)
+
+top.component_function()
